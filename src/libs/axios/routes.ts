de0a3c2f322a5bb1f0b1517a22d28810bdcf7144
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-11-06 18:52:18 +0330

    feat(types): added types to DTO and axios responses

--- src/libs/axios/routes.ts
+++ src/libs/axios/routes.ts
@@ -1,161 +1,109 @@
 import api from "./api"
+import {
+	AboutUsResponse,
+	AllSourcesResponse,
+	ChatHistoryResponse,
+	ChatMessagesResponse,
+	ContactUsResponse,
+	HomeResponse,
+	LessonFlashcardsResponse,
+	LessonSlidersResponse,
+	LessonVideosResponse,
+	LoginStudentResponse,
+	NewChatResponse,
+	NotificationsResponse,
+	OneSourceResponse,
+	SendMessageResponse,
+	SettingsResponse,
+	WeeklyScheduleResponse,
+} from "./dto"
 
 // pages (auth needed)
-export const getHome = async () => {
-	try {
-		const res = await api.get("/pages/home")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getHome = async (): Promise<HomeResponse> => {
+	return await api.get("/pages/home")
 }
 
-export const getAllSources = async () => {
-	try {
-		const res = await api.get("/pages/sources")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getAllSources = async (): Promise<AllSourcesResponse> => {
+	return await api.get("/pages/sources")
 }
 
-export const getOneSource = async (source: number | string) => {
-	try {
-		const res = await api.get(`/pages/sources/${source}`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getOneSource = async (
+	source: number | string,
+): Promise<OneSourceResponse> => {
+	return await api.get(`/pages/sources/${source}`)
 }
 
-export const getLessonSliders = async (lesson: number | string) => {
-	try {
-		const res = await api.get(`/pages/sources/lessons/${lesson}/sliders`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getLessonSliders = async (
+	lesson: number | string,
+): Promise<LessonSlidersResponse> => {
+	return await api.get(`/pages/sources/lessons/${lesson}/sliders`)
 }
 
-export const getLessonVideos = async (lesson: number | string) => {
-	try {
-		const res = await api.get(`/pages/sources/lessons/${lesson}/videos`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getLessonVideos = async (
+	lesson: number | string,
+): Promise<LessonVideosResponse> => {
+	return await api.get(`/pages/sources/lessons/${lesson}/videos`)
 }
 
-export const getLessonFlashcards = async (lesson: number | string) => {
-	try {
-		const res = await api.get(`/pages/sources/lessons/${lesson}/flashcards`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getLessonFlashcards = async (
+	lesson: number | string,
+): Promise<LessonFlashcardsResponse> => {
+	return await api.get(`/pages/sources/lessons/${lesson}/flashcards`)
 }
 
-export const getWeeklySchedule = async () => {
-	try {
-		const res = await api.get("/pages/weekly-schedule")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getWeeklySchedule = async (): Promise<WeeklyScheduleResponse> => {
+	return await api.get("/pages/weekly-schedule")
 }
 
-export const getNotifications = async () => {
-	try {
-		const res = await api.get("/pages/notifications")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getNotifications = async (): Promise<NotificationsResponse> => {
+	return await api.get("/pages/notifications")
 }
 
-export const getSettings = async () => {
-	try {
-		const res = await api.get("/pages/settings")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getSettings = async (): Promise<SettingsResponse> => {
+	return await api.get("/pages/settings")
 }
 
-export const getChatHistory = async () => {
-	try {
-		const res = await api.get("/pages/chat/history")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getChatHistory = async (): Promise<ChatHistoryResponse> => {
+	return await api.get("/pages/chat/history")
 }
 
-export const getChatMessages = async (chat: number | string) => {
-	try {
-		const res = await api.get(`/pages/chat/${chat}`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getChatMessages = async (
+	chat: number | string,
+): Promise<ChatMessagesResponse> => {
+	return await api.get(`/pages/chat/${chat}`)
 }
 
 // pages (no auth needed)
-export const getAboutUs = async () => {
-	try {
-		const res = await api.get("/pages/about-us")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getAboutUs = async (): Promise<AboutUsResponse> => {
+	return await api.get("/pages/about-us")
 }
 
-export const getContactUs = async () => {
-	try {
-		const res = await api.get("/pages/contact-us")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const getContactUs = async (): Promise<ContactUsResponse> => {
+	return await api.get("/pages/contact-us")
 }
 
 // student auth
-export const login = async (data: { username?: string; password?: string }) => {
-	try {
-		const res = await api.post("/auth/login", data)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const loginStudent = async (data: {
+	username?: string
+	password?: string
+}): Promise<LoginStudentResponse> => {
+	return await api.post("/auth/login", data)
 }
 
-export const logout = async () => {
-	try {
-		const res = await api.post("/auth/logout")
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const logoutStudent = async () => {
+	return await api.post("/auth/logout")
 }
 
 // chat
-export const createNewChat = async (type: "casual" | "quiz") => {
-	try {
-		const res = await api.post(`/chat/${type}`)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+export const createNewChat = async (
+	type: "casual" | "quiz",
+): Promise<NewChatResponse> => {
+	return await api.post(`/chat/${type}`)
 }
 
 export const sendMessageToChat = async (
 	chat: number | string,
 	data: FormData | { content: string },
-) => {
-	try {
-		const res = await api.post(`/chat/${chat}/message`, data)
-		return res.data
-	} catch (error) {
-		console.log(error)
-	}
+): Promise<SendMessageResponse> => {
+	return await api.post(`/chat/${chat}/message`, data)
 }
