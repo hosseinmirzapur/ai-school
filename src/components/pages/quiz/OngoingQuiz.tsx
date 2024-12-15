c7e7363bf618a98eef884d748f48bd2090ca4e95
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-12-15 18:33:50 +0330

    feat(quiz): bg image for ongoing quiz box

--- src/components/pages/quiz/OngoingQuiz.tsx
+++ src/components/pages/quiz/OngoingQuiz.tsx
@@ -4,6 +4,7 @@ import { IQuiz } from "@/types"
 import { motionFX } from "@/utils"
 import { Card, CardBody } from "@nextui-org/react"
 import { motion } from "framer-motion"
+import Image from "next/image"
 
 interface IProps {
 	quizzes: IQuiz[]
@@ -15,10 +16,17 @@ const OngoingQuiz: React.FC<IProps> = () => {
 			initial={motionFX.initial}
 			animate={motionFX.animate}
 			transition={motionFX.transition}
+			className="flex items-center justify-center h-full"
 		>
-			<Card isPressable isHoverable>
-				<CardBody>
-					<span>برای شرکت در آزمون کلیک کنید</span>
+			<Card isPressable isHoverable className="bg-secondary-100">
+				<CardBody className="flex flex-col gap-3 items-center justify-center h-[300px] w-[300px]">
+					<Image
+						src={"/assets/quiz.png"}
+						alt="quiz-icon"
+						width={200}
+						height={200}
+					/>
+					<span className="text-lg">برای شرکت در آزمون کلیک کنید</span>
 				</CardBody>
 			</Card>
 		</motion.div>
